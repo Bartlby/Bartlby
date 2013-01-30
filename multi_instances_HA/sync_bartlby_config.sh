d6bec06f87c1caa18dd2b1e697c0f28a0a59d6ea
hjanuschka <helmut@januschka.com>
2013-01-30 11:08:09 +0100

    shm sync

--- multi_instances_HA/sync_bartlby_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#get config from master
-ssh -i /storage/SF.NET/BARTLBY/priv  root@januschka.com -C "mysqldump  -u root --password=volLKorn8BROt4  bartlby_second > /root/1.mysql"
-scp -i /storage/SF.NET/BARTLBY/priv root@januschka.com:/root/1.mysql /root/1.mysql
-
-mysql  -u root --password=volLKorn8BROt4 bartlbydev < /root/1.mysql
-/storage/SF.NET/BARTLBY/GIT/bartlby-core/bartlby_shmt  /storage/SF.NET/BARTLBY/GIT/bartlby-core/BARTLBY.local reload
-
