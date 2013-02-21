cc93a1bee7f6140f6ed081635ecbaaf8ac936754
hjanuschka <helmut@januschka.com>
2013-02-21 11:52:20 +0100

    updated modules

--- update_repository_with_latest_module_commits.sh
+++ update_repository_with_latest_module_commits.sh
@@ -1,1 +1,1 @@
-git submodule  foreach git pull  origin master && git commit -a -m "updated modules" && git push
+git submodule  foreach git checkout master && git pull  origin master && git commit -a -m "updated modules" && git push
